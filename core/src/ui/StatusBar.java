05510f1b82bd78aee28e95df111d51cb1dc914c5
jootnet <shawryan@outlook.com>
2022-04-20 22:43:50 +0800

    game scene bottom status bar

---
+++ core/src/ui/StatusBar.java
@@ -0,0 +1,53 @@
+package ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
+
+import joot.m2.client.image.M2Texture;
+import joot.m2.client.util.AssetUtil;
+
+/**
+ * 
+ * 游戏界面下方的状态栏
+ * 
+ * @author linxing
+ *
+ */
+public final class StatusBar extends WidgetGroup {
+	/** 右侧功能区背景图 */
+	private Image funAreaBg;
+	/** 药品随机卷使用栏背景图 */
+	private Image quikAreaBg;
+	/** 能量条背景图 */
+	private Image powerBg;
+
+	public StatusBar() {
+		while(!AssetUtil.isLoaded("newopui/0")) AssetUtil.update();
+		while(!AssetUtil.isLoaded("newopui/1")) AssetUtil.update();
+		while(!AssetUtil.isLoaded("newopui/2")) AssetUtil.update();
+		while(!AssetUtil.isLoaded("newopui/3")) AssetUtil.update();
+		addActor(new Image(AssetUtil.<M2Texture>get("newopui/0"))); // 左侧生命值法力值的背景图s
+		addActor((funAreaBg = new Image(AssetUtil.<M2Texture>get("newopui/1"))));
+		addActor((quikAreaBg = new Image(AssetUtil.<M2Texture>get("newopui/2"))));
+		quikAreaBg.setPosition(180, 157);
+		addActor((powerBg = new Image(AssetUtil.<M2Texture>get("newopui/3"))));
+	}
+	
+	/*@Override
+	public void draw(Batch batch, float parentAlpha) {
+		super.draw(batch, parentAlpha);
+	}*/
+	
+	/*@Override
+	public Actor hit(float x, float y, boolean touchable) {
+		// TODO 把镂空的地方返回false，鼠标点选之后可以走跑
+		return super.hit(x, y, touchable);
+	}*/
+
+	@Override
+	public void layout() {
+		funAreaBg.setX(getWidth() - funAreaBg.getWidth());
+		quikAreaBg.setX((getWidth() - quikAreaBg.getWidth()) / 2);
+		powerBg.setX(getWidth() - funAreaBg.getWidth() + 6);
+	}
+}
